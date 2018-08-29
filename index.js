772908e8a0944d2b4adcb425e0f671cd416ad9ad
Mike Lynch <mike@mikelynch.org>
2018-08-29 14:36:32 +1000

    Changed the index.js so that it re-exports the libraries

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-#!/usr/bin/env node
-console.log('Hello, world!');
-var fs = require('fs');
-var program = require('commander');
-var XLSX = require('xlsx');
-var path = require('path');
-const context = require("./defaults/context.json");
-const Collection = require("./collection.js")
 
 
- program
- .arguments('<dir>')
- .option('-r, --recursive', 'Process sub-directories')
- .action(function(dir){
-            var collection = new Collection;
-            collection.read(dir, program, is_root = true);
-            console.log("METADATA!!!!!!!!!!!!!!!", collection.collection_metadata)
-          })
- .parse(process.argv);
+module.exports = {
+	Collection: require('./lib/collection.js'),
+	Index: require('./lib/index_html.js'),
+	Datacite: require('./lib/datacite.js'),
+	Bag: require('./lib/bag.js')
+}
+
