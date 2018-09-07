e010fbf73d036901cac6dc6ac6fbd4a01b0eded5
Mike Lynch <mike@mikelynch.org>
2018-09-07 12:18:23 +1000

    Checking in the typescript declaration files

---
+++ property.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for calcyte 0.0.2
+// Project: DataCrate
+// Definitions by: Mike Lynch
+
+
+declare class Collection {
+    constructor();
+
+    property: any;
+    method(any): void;
+}
+
+
